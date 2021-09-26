3964dcb137c2f6e588dab987de81f2a3ee83da60
Sotelo <cgonzalezsotelo@gmail.com>
2021-09-26 18:06:46 -0300

    no logro imprimir en la vista al filtrar con el buscador :C

--- src/components/Busqueda.vue
+++ src/components/Busqueda.vue
@@ -10,34 +10,9 @@
           {{ zapatilla.precio }}
         </li>
       </ul>
-      <ul v-else>
-        <li>Escribe Algo para buscar</li>
-      </ul>
+      <ul v-else></ul>
     </div>
   </div>
-  <!-- <div class="card mb-3">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <pre>{{ $store.getters.buscarZapatilla }} hola</pre>
-        <img
-          :src="zapatillas.imagen"
-          class="img-fluid rounded-start"
-          :alt="zapatillas.nombre"
-        />
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5
-            class="card-title"
-            v-for="zapatilla in $store.getters.buscarZapatilla"
-            :key="zapatilla.nombre"
-          >
-            {{ zapatilla.nombre }}
-          </h5>
-        </div>
-      </div>
-    </div>
-  </div> -->
 </template>
 <script>
 export default {
